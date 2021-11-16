7d9d6c9453ed78d3a2e7d7ee81c6e15fd974abec
Joao Trindade <jtrindade@cc.isel.ipl.pt>
2021-11-16 11:19:10 +0000

    Lecture 12 - WebApp - webapi (cont.)

---
+++ 12 - WebApp - MyBooks/aliche/app-data-int-mem.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const errors = require('./app-errors');
+
+const books = {};
+
+const hasBook = async (bookId) => !!books[bookId];
+
+async function saveBook(bookObj) {
+	const bookId = bookObj.id;
+	books[bookId] = bookObj;
+	return bookId;
+}
+
+async function loadBook(bookId) {
+	const bookObj = books[bookId];
+	if (!bookObj) {
+		const err = errors.NOT_FOUND({ id: bookId })
+		throw err;
+	}
+	return bookObj;
+}
+
+async function deleteBook(bookId) {
+	const bookObj = books[bookId];
+	if (!bookObj) {
+		throw errors.NOT_FOUND({ id: bookId });
+	}
+	delete books[bookId];
+	return bookId;
+}
+
+async function listBooks() {
+	return Object.values(books);
+}
+
+module.exports = {
+	hasBook,
+	saveBook,
+	loadBook,
+	deleteBook,
+	listBooks
+};
